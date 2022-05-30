4ccb5c4c8ddd72c5fce74618703582a06cb663d4
cheonyongjun <yes1312@naver.com>
2022-05-30 23:56:11 +0900

    data and register

--- workspace_assembly/HelloWorld.asm
+++ workspace_assembly/HelloWorld.asm
@@ -3,7 +3,8 @@
 section .text
 global CMAIN
 CMAIN:
-    mov rbp, rsp; for correct debugging
+        
+        mov rbp, rsp; for correct debugging
 
     ; 8bit = 1 byte
     ; 16bit = 2 bytes = 1 word
@@ -18,10 +19,36 @@ CMAIN:
     mov cl, 0xff
     
     mov al, 0x00
-    mov rax, rdx
     
+    ; 메모리 <-> 레지스터
+    ;mov rax, a ; a라는 바구니의 주소 값을 rax에 복사
+    ;mov rax, [a] ; a라는 바구니 안에 있는 값을 rax에 복
+    mov al, [a]
     
+    mov [a], byte 0x55 
+    mov [a], word 0x6666
+    mov [a], cl
+
     xor rax, rax
     ret
     
-;section.data
+    ; 변수의 선언 및 사용
+    ; 변수는 그냥 데이터를 저장하는 바구니 [ ]
+    ; - 처음에 바구니 사용하겠다 선언 (이름과 크기를 지정)
+    
+    ; 초기화 된 데이터
+    ; [변수이름] [크기] [초기값]
+    ; [크기] db(1) dw(2) dd(4) dq(8)
+    
+section .data
+    a db 0x11 ; [0x11]
+    b dw 0x2222
+    c dd 0x33333333
+    d dq 0x4444444444444444
+    
+    ; 초기화 되지 않은 데이터
+    ; [변수이름] [크기] [초기값]
+    ; [크기] resb(1) resw(2) resd(4) resq(8)
+section .bss
+    e resb 10
+    

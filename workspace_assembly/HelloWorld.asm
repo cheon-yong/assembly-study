2cea49f57bbe1164e38f8c14e4a948f1157e4fb6
cheonyongjun <yes1312@naver.com>
2022-06-07 23:50:33 +0900

    function

--- workspace_assembly/HelloWorld.asm
+++ workspace_assembly/HelloWorld.asm
@@ -5,33 +5,53 @@ global CMAIN
 CMAIN:        
     mov rbp, rsp; for correct debugging
 
-    ; 배열과 주소
+    ; 함수 (프로시저 procedure 서브루틴 subroutine)
     
-    ; 배열 : 동일한 타입의 데이터 묶음
-    ; - 배열을 구성하는 각 값을 배열 요소(element)라고 함
-    ; - 배열의 위치를 가리키는 숫자를 인덱스(index)라고 함
+    ; call PRINT_MSG
     
-    mov rax, a
-    
-    ; 연습문제 : a배열의 모든 데이터를 출력하기
-    xor ecx, ecx
-LABEL_PRINT_A:
-    ;PRINT_HEX 1, [a+ecx]
-    ;NEWLINE
-    inc ecx; add ecx, 1
-    cmp ecx, 5
-    jne LABEL_PRINT_A
-    
-    xor ecx, ecx
-LABEL_PRINT_B:
-    PRINT_HEX 2, [b+ecx*2]
+    mov eax, 10
+    mov ebx, 15
+    call MAX
+    PRINT_DEC 4, ecx
     NEWLINE
-    inc ecx
-    cmp ecx, 5
-    jne LABEL_PRINT_B    
     
     xor rax, rax
     ret
+    
+PRINT_MSG:
+    PRINT_STRING msg
+    NEWLINE
+    ret
+; ex) 두 값 중 더 큰 값을 반환하는 max
+; 근데 두 값을 어떻게 넘겨받지? 반환은?
+; eax와 ebx 입력값을 ecx에 반환
+
+MAX:
+    cmp eax, ebx;
+    jg L1
+    mov ecx, ebx;
+    jmp L2
+L1:
+    mov ecx, eax
+L2:
+    ret
+    
+    ; 인자가 10개라면?
+    ; eax, ebx에 이미 중요한 값을 사용하고 있으면?
+    ; [!] .data .bss 사용하면?
+    ; 인자를 몇개를 할당해야할까
+    
+    ; 다른 메모리 구조가 필요
+    ; - 꿈이 유효한 동안에는 꿈을 유지시켜야함 (유효 범위의 개념이 필요)
+    ; - 꿈이 끝나면 그 꿈을 부셔버려도 됨 (정리의 개념)
+    ; - 꿈에서도 또 꿈을 꿀 수 있다는 것을 고려해야 함 (유동적으로 유효 범위가 확장 가능)
+    
+    
+    ; [!] 스택(stack)이라는 메모리 영역을 사용
+    ; 함수가 사용하는 일종의 메모장
+    ; - 매개 변수 전달
+    ; - 돌아갈 주소 관리
+    
     
     ; 변수의 선언 및 사용
     ; 변수는 그냥 데이터를 저장하는 바구니 [ ]
